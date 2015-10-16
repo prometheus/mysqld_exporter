f35c189e40a7b0ad0686fd002f3935a8a62f6edd
SuperQ <superq@gmail.com>
2015-10-16 13:59:37 +0200

    Fix perfTableLockWaitsQuery parsing

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -137,28 +137,29 @@ const (
 		`
 	perfTableLockWaitsQuery = `
 		SELECT
-		    OBJECT_SCHEMA, OBJECT_NAME,
+		    OBJECT_SCHEMA,
+		    OBJECT_NAME,
 		    COUNT_READ_NORMAL,
 		    COUNT_READ_WITH_SHARED_LOCKS,
 		    COUNT_READ_HIGH_PRIORITY,
 		    COUNT_READ_NO_INSERT,
-		    COUNT_WRITE_NORMAL,
+		    COUNT_READ_EXTERNAL,
 		    COUNT_WRITE_ALLOW_WRITE,
 		    COUNT_WRITE_CONCURRENT_INSERT,
 		    COUNT_WRITE_DELAYED,
 		    COUNT_WRITE_LOW_PRIORITY,
-		    COUNT_READ_EXTERNAL,
-		    COUNT_WRITE_EXTERNAL
+		    COUNT_WRITE_NORMAL,
+		    COUNT_WRITE_EXTERNAL,
 		    SUM_TIMER_READ_NORMAL,
 		    SUM_TIMER_READ_WITH_SHARED_LOCKS,
 		    SUM_TIMER_READ_HIGH_PRIORITY,
 		    SUM_TIMER_READ_NO_INSERT,
-		    SUM_TIMER_WRITE_NORMAL,
+		    SUM_TIMER_READ_EXTERNAL,
 		    SUM_TIMER_WRITE_ALLOW_WRITE,
 		    SUM_TIMER_WRITE_CONCURRENT_INSERT,
 		    SUM_TIMER_WRITE_DELAYED,
 		    SUM_TIMER_WRITE_LOW_PRIORITY,
-		    SUM_TIMER_READ_EXTERNAL,
+		    SUM_TIMER_WRITE_NORMAL,
 		    SUM_TIMER_WRITE_EXTERNAL
 		  FROM performance_schema.table_lock_waits_summary_by_table
 		  WHERE OBJECT_SCHEMA NOT IN ('mysql', 'performance_schema', 'information_schema')
@@ -981,32 +982,58 @@ func scrapePerfTableLockWaits(db *sql.DB, ch chan<- prometheus.Metric) error {
 	defer perfSchemaTableLockWaitsRows.Close()
 
 	var (
-		objectSchema, objectName                      string
-		countReadNormal, countReadWithSharedLocks     uint64
-		countReadHighPriority, countReadNoInsert      uint64
-		countWriteNormal, countWriteAllowWrite        uint64
-		countWriteConcurrentInsert, countWriteDelayed uint64
-		countWriteLowPriority                         uint64
-		countReadExternal, countWriteExternal         uint64
-		timeReadNormal, timeReadWithSharedLocks       uint64
-		timeReadHighPriority, timeReadNoInsert        uint64
-		timeWriteNormal, timeWriteAllowWrite          uint64
-		timeWriteConcurrentInsert, timeWriteDelayed   uint64
-		timeWriteLowPriority                          uint64
-		timeReadExternal, timeWriteExternal           uint64
+		objectSchema               string
+		objectName                 string
+		countReadNormal            uint64
+		countReadWithSharedLocks   uint64
+		countReadHighPriority      uint64
+		countReadNoInsert          uint64
+		countReadExternal          uint64
+		countWriteAllowWrite       uint64
+		countWriteConcurrentInsert uint64
+		countWriteDelayed          uint64
+		countWriteLowPriority      uint64
+		countWriteNormal           uint64
+		countWriteExternal         uint64
+		timeReadNormal             uint64
+		timeReadWithSharedLocks    uint64
+		timeReadHighPriority       uint64
+		timeReadNoInsert           uint64
+		timeReadExternal           uint64
+		timeWriteAllowWrite        uint64
+		timeWriteConcurrentInsert  uint64
+		timeWriteDelayed           uint64
+		timeWriteLowPriority       uint64
+		timeWriteNormal            uint64
+		timeWriteExternal          uint64
 	)
 
 	for perfSchemaTableLockWaitsRows.Next() {
 		if err := perfSchemaTableLockWaitsRows.Scan(
-			&objectSchema, &objectName,
-			&countReadNormal, &countReadWithSharedLocks,
-			&countReadHighPriority, &countReadNoInsert, &countWriteNormal,
-			&countWriteAllowWrite, &countWriteConcurrentInsert, &countWriteDelayed,
-			&countWriteLowPriority, &countReadExternal, &countWriteExternal,
-			&timeReadNormal, &timeReadWithSharedLocks,
-			&timeReadHighPriority, &timeReadNoInsert, &timeWriteNormal,
-			&timeWriteAllowWrite, &timeWriteConcurrentInsert, &timeWriteDelayed,
-			&timeWriteLowPriority, &timeReadExternal, &timeWriteExternal,
+			&objectSchema,
+			&objectName,
+			&countReadNormal,
+			&countReadWithSharedLocks,
+			&countReadHighPriority,
+			&countReadNoInsert,
+			&countReadExternal,
+			&countWriteAllowWrite,
+			&countWriteConcurrentInsert,
+			&countWriteDelayed,
+			&countWriteLowPriority,
+			&countWriteNormal,
+			&countWriteExternal,
+			&timeReadNormal,
+			&timeReadWithSharedLocks,
+			&timeReadHighPriority,
+			&timeReadNoInsert,
+			&timeReadExternal,
+			&timeWriteAllowWrite,
+			&timeWriteConcurrentInsert,
+			&timeWriteDelayed,
+			&timeWriteLowPriority,
+			&timeWriteNormal,
+			&timeWriteExternal,
 		); err != nil {
 			return err
 		}

9c23e95b0b347fc96225d4e4625ede50d4e61740
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2017-08-28 11:44:06 +0300

    Add very basic integration test as an example.

---
+++ collector/exporter_test.go
@@ -0,0 +1,47 @@
+package collector
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/model"
+	"github.com/smartystreets/goconvey/convey"
+)
+
+const dsn = "root@/mysql"
+
+func TestExporter(t *testing.T) {
+	if testing.Short() {
+		t.Skip("-short is passed, skipping test")
+	}
+
+	exporter := New(dsn, Collect{
+		GlobalStatus: true,
+	})
+
+	convey.Convey("Metrics describing", t, func() {
+		ch := make(chan *prometheus.Desc)
+		go func() {
+			exporter.Describe(ch)
+			close(ch)
+		}()
+
+		for range ch {
+		}
+	})
+
+	convey.Convey("Metrics collection", t, func() {
+		ch := make(chan prometheus.Metric)
+		go func() {
+			exporter.Collect(ch)
+			close(ch)
+		}()
+
+		for m := range ch {
+			got := readMetric(m)
+			if got.labels[model.MetricNameLabel] == "mysql_up" {
+				convey.So(got.value, convey.ShouldEqual, 1)
+			}
+		}
+	})
+}

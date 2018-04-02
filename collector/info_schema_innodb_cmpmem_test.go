ff49996ee0b929a420d07c4beec1074c8f44f82f
Vadim Yalovets <vadim.yalovets@percona.com>
2018-04-02 14:45:36 +0300

    PMM-2019 Moved buffer from values to parameters

--- collector/info_schema_innodb_cmpmem_test.go
+++ collector/info_schema_innodb_cmpmem_test.go
@@ -16,9 +16,9 @@ func TestScrapeInnodbCmpMem(t *testing.T) {
 	}
 	defer db.Close()
 
-	columns := []string{"page_size", "page_size", "buffer_pool_instance", "pages_used", "pages_free", "relocation_ops", "relocation_time"}
+	columns := []string{"page_size", "buffer", "pages_used", "pages_free", "relocation_ops", "relocation_time"}
 	rows := sqlmock.NewRows(columns).
-		AddRow("1024", 10, 20, 30, 40, 50, 60)
+		AddRow("1024", "0", 30, 40, 50, 60)
 	mock.ExpectQuery(sanitizeQuery(innodbCmpMemQuery)).WillReturnRows(rows)
 
 	ch := make(chan prometheus.Metric)
@@ -30,12 +30,10 @@ func TestScrapeInnodbCmpMem(t *testing.T) {
 	}()
 
 	expected := []MetricResult{
-		{labels: labelMap{"page_size": "1024"}, value: 10, metricType: dto.MetricType_COUNTER},
-		{labels: labelMap{"page_size": "1024"}, value: 20, metricType: dto.MetricType_COUNTER},
-		{labels: labelMap{"page_size": "1024"}, value: 30, metricType: dto.MetricType_COUNTER},
-		{labels: labelMap{"page_size": "1024"}, value: 40, metricType: dto.MetricType_COUNTER},
-		{labels: labelMap{"page_size": "1024"}, value: 50, metricType: dto.MetricType_COUNTER},
-		{labels: labelMap{"page_size": "1024"}, value: 60, metricType: dto.MetricType_COUNTER},
+		{labels: labelMap{"page_size": "1024", "buffer": "0"}, value: 30, metricType: dto.MetricType_COUNTER},
+		{labels: labelMap{"page_size": "1024", "buffer": "0"}, value: 40, metricType: dto.MetricType_COUNTER},
+		{labels: labelMap{"page_size": "1024", "buffer": "0"}, value: 50, metricType: dto.MetricType_COUNTER},
+		{labels: labelMap{"page_size": "1024", "buffer": "0"}, value: 0.06, metricType: dto.MetricType_COUNTER},
 	}
 	convey.Convey("Metrics comparison", t, func() {
 		for _, expect := range expected {

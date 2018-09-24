6bf0ec9e8b4c5c4f70a176a9748ed895e6fee38f
Cody Taylor <cody@codytaylor.org>
2018-09-24 15:23:14 -0400

    Use GLOBAL to prevent mysql deadlock
    Signed-off-by: Cody Taylor <cody@codytaylor.org>

--- collector/collector.go
+++ collector/collector.go
@@ -15,7 +15,7 @@ const (
 	// Math constant for picoseconds to seconds.
 	picoSeconds = 1e12
 	// Query to check whether user/table/client stats are enabled.
-	userstatCheckQuery = `SHOW VARIABLES WHERE Variable_Name='userstat'
+	userstatCheckQuery = `SHOW GLOBAL VARIABLES WHERE Variable_Name='userstat'
 		OR Variable_Name='userstat_running'`
 )
 

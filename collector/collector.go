22c9765e0ea369bbd8419c8688f4422984250d65
Vadim Yalovets <vadim.yalovets@percona.com>
2020-06-01 16:41:31 +0300

    Fixed output value of wsrep_cluster_status (#473)
    * Fixed output value of wsrep_cluster_status
    
    Signed-off-by: adivinho <vadim.yalovets@percona.com>

--- collector/collector.go
+++ collector/collector.go
@@ -18,6 +18,7 @@ import (
 	"database/sql"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -52,11 +53,11 @@ func parseStatus(data sql.RawBytes) (float64, bool) {
 	if bytes.Equal(data, []byte("Connecting")) {
 		return 0, true
 	}
-	// SHOW GLOBAL STATUS like 'wsrep_cluster_status' can return "Primary" or "Non-Primary"/"Disconnected"
+	// SHOW GLOBAL STATUS like 'wsrep_cluster_status' can return "Primary" or "non-Primary"/"Disconnected"
 	if bytes.Equal(data, []byte("Primary")) {
 		return 1, true
 	}
-	if bytes.Equal(data, []byte("Non-Primary")) || bytes.Equal(data, []byte("Disconnected")) {
+	if strings.EqualFold(string(data), "non-Primary") || bytes.Equal(data, []byte("Disconnected")) {
 		return 0, true
 	}
 	if logNum := logRE.Find(data); logNum != nil {

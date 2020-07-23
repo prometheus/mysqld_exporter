1834ab2f8429aa553110046ce384aef30117d467
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2020-07-23 12:20:19 +0300

    PMM-5783 Fix typo.

--- collector/slave_status_test.go
+++ collector/slave_status_test.go
@@ -19,9 +19,9 @@ func TestScrapeSlaveStatus(t *testing.T) {
 	defer db.Close()
 
 	versionColumns := []string{"@@version", "@@version_comment"}
-	vesionRows := sqlmock.NewRows(versionColumns).
+	versionRows := sqlmock.NewRows(versionColumns).
 		AddRow("8.0.21", "MySQL Community Server - GPL")
-	mock.ExpectQuery(sanitizeQuery("SELECT @@version, @@version_comment")).WillReturnRows(vesionRows)
+	mock.ExpectQuery(sanitizeQuery("SELECT @@version, @@version_comment")).WillReturnRows(versionRows)
 
 	columns := []string{"Master_Host", "Read_Master_Log_Pos", "Slave_IO_Running", "Slave_SQL_Running", "Seconds_Behind_Master"}
 	rows := sqlmock.NewRows(columns).

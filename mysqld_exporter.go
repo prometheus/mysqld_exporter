e0a9caef4b94f0114eb480912abbfb344862381b
Pavel Zinovkin <pzinovkin@gmail.com>
2020-09-10 17:10:48 +0300

    Fixes typo in log message
    Fixes #464

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -261,7 +261,7 @@ func main() {
 </html>
 `)
 
-	level.Info(logger).Log("msg", "Starting msqyld_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Starting mysqld_exporter", "version", version.Info())
 	level.Info(logger).Log("msg", "Build context", version.BuildContext())
 
 	dsn = os.Getenv("DATA_SOURCE_NAME")

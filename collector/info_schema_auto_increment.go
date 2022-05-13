2fff30c1efee8418337863df6f4771305ec90b2e
Taras Shcherban <taras.shcherban@implex.dev>
2022-05-13 12:08:50 +0300

    PMM-2038 STRAIGHT_JOIN query fix

--- collector/info_schema_auto_increment.go
+++ collector/info_schema_auto_increment.go
@@ -25,7 +25,7 @@ import (
 
 // https://jira.percona.com/browse/PMM-4001 explains STRAIGHT_JOIN usage.
 const infoSchemaAutoIncrementQuery = `
-		SELECT t.table_schema, t.table_name, column_name, ` + "`auto_increment`" + `,
+		SELECT t.table_schema, t.table_name, column_name, auto_increment,
 		  pow(2, case data_type
 		    when 'tinyint'   then 7
 		    when 'smallint'  then 15
@@ -34,7 +34,8 @@ const infoSchemaAutoIncrementQuery = `
 		    when 'bigint'    then 63
 		    end+(column_type like '% unsigned'))-1 as max_int
 		  FROM information_schema.columns c
-		  STRAIGHT_JOIN information_schema.tables t USING (table_schema,table_name)
+		  STRAIGHT_JOIN information_schema.tables t
+		    ON BINARY t.table_schema = c.table_schema AND BINARY t.table_name = c.table_name
 		  WHERE c.extra = 'auto_increment' AND t.auto_increment IS NOT NULL
 		`
 
